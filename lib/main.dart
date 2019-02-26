7ef6652a516754730b0ebe23f04c7aaa75f9acf7
MatanSafri <matansafwork@gmail.com>
2019-02-26 13:00:12 +0200

    Login page without auth

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'login_signup_page.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Flutter Login Demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new LoginSignUpPage());
+  }
+}
